6a43acfc57fa28446660255246e3327f84ca1c22
Sam McKay <samm@aspsys.com>
2024-09-23 11:12:44 -0600

    modify the App.js file to allow props to be passed

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import AppBar from "./Components/AppBar";
 import { useState, useEffect } from 'react';
 
 function App() {
+  const rows = [
+    { id: 1, A: "c1", Customer: "P&G", Nodename:"node1", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
+    { id: 2, A: "c2", Customer: "P&G", NodeName: "node2", JobNum: "124", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
+  ];
 
   return (
     <div className="App">
       <AppBar id="AppBar"/>
-      <DataTable title={"AIME Control Table"} />
+      <DataTable info={rows} title={"AIME Control Table"} />
       
     </div>
   );

c7c54674459387bec50ecad1f8f58de8cb8cac3d
Sam McKay <samm@aspsys.com>
2024-09-23 09:36:24 -0600

    Make changes to the App.js file to pass props into enhanced table

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 
 import EnhancedTable from "./Components/EnhancedTable";
 import AppBar from "./Components/AppBar";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [rows, setRows] = useState([]);
+  const addNodeToList = (newNode) => {
+    setRows([...rows, newNode]);
+  };
+  
+  useEffect(() => {
+    // Call the function only once when the component mounts
+    addNodeToList([18902, "Procter and Gamble", "node1", 5455, "idle", "1.0.0.0", "1.0.0.0.0", "1.0.0.0", "ok", "ok"]);
+  }, []); // Empty dependency array means this runs once
+
   return (
     <div className="App">
       <AppBar id="AppBar"/>
-      <EnhancedTable/>
+      <EnhancedTable {...rows} />
+      
     </div>
   );
 }

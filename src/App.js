b749248a7c53bae6f8ebebf898f16c53d766e50e
Sam McKay <samm@aspsys.com>
2024-09-23 09:56:05 -0600

    properly passing props into the enhancedTable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
 
 import EnhancedTable from "./Components/EnhancedTable";
+import React from 'react';
 import AppBar from "./Components/AppBar";
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [rows, setRows] = useState([]);
+  const [nodeList, setNodeList] = React.useState([""]);
   const addNodeToList = (newNode) => {
-    setRows([...rows, newNode]);
+    console.log(newNode);
+    setNodeList([...nodeList, newNode]);
   };
   
   useEffect(() => {
     // Call the function only once when the component mounts
+    addNodeToList([18902, "Procter and Gamble", "node1", 5455, "idle", "1.0.0.0", "1.0.0.0.0", "1.0.0.0", "ok", "ok"]);
     addNodeToList([18902, "Procter and Gamble", "node1", 5455, "idle", "1.0.0.0", "1.0.0.0.0", "1.0.0.0", "ok", "ok"]);
   }, []); // Empty dependency array means this runs once
 
   return (
     <div className="App">
       <AppBar id="AppBar"/>
-      <EnhancedTable {...rows} />
+      <EnhancedTable {...nodeList} />
       
     </div>
   );

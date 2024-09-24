91e14c71a5b7e70fbd070252beb3c1cdb23c5998
Sam McKay <sammckay31@gmail.com>
2024-09-24 16:05:17 -0600

    add a new page which will serve as the template for node pages

---
+++ src/pages/Nodepage.js
@@ -0,0 +1,15 @@
+import AppBar from '../Components/AppBar';
+
+
+function BlankAime() {
+
+    return (
+        <div>
+            <title>fdfsd</title>
+            <AppBar></AppBar>
+
+        </div>
+    )
+}
+
+export default BlankAime;

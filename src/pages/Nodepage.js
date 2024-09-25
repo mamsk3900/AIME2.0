1234ed11b230ae347f25e583a0289992c83f67f2
Sam McKay <sammckay31@gmail.com>
2024-09-25 11:39:13 -0600

    Add the clock to the node page and add the unique appbar

--- src/pages/Nodepage.js
+++ src/pages/Nodepage.js
@@ -1,17 +1,46 @@
-import { useEffect } from 'react';
-import AppBar from '../Components/AppBar';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import NodePageAppBar from '../Components/NodePageAppBar'
 
 
-function BlankAime() {
+
+function NodePage() {
     useEffect(() => {
         document.title = "NodePage";
+
+        const intervalID = setInterval(() => {
+            let date = new Date();
+            let dateSpan = document.getElementById("dateSpan")
+    
+            const options = 
+                {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                    hour12: true,
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: "numeric"
+                };
+    
+            const formatter = new Intl.DateTimeFormat('en-US', options);
+            const formattedDate = formatter.format(date);
+    
+            const result = `${formattedDate.replace(/at/, '')}`;
+            dateSpan.innerHTML = result;
+        })
+        return () => clearInterval(intervalID);
     }, [])
 
     return (
-        <div id="page">
-            <div>fdsaffd</div>
+
+        <div id="NodePage">
+            <NodePageAppBar></NodePageAppBar>
+            <div id="clock">
+                <span id="dateSpan">January 01, 1999 at 00:00:00 AM</span>
+            </div>
         </div>
     )
 }
 
-export default BlankAime;
+export default NodePage;

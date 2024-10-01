b8b5bfe239fd14b82081f6046f4526710a238cfe
Sam McKay <sammckay31@gmail.com>
2024-10-01 14:06:39 -0600

    Add the class creation to the Home page. I might end up putting these functions into another file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,106 @@ import React from 'react';
 import DataTable from "../Components/DataTable"
 import AppBar from "../Components/AppBar";
 import { useState, useEffect } from 'react';
+import { SingleNode } from '../Classes/SingleNode';
+import {CPU} from '../Classes/CPU';
+import { TestParent } from '../Classes/TestParent';
+import { BOM } from '../Classes/BOM';
+import { System } from '../Classes/System';
+import { MemInfo } from '../Classes/MemInfo';
+import {DIMM} from '../Classes/DIMM';
+import {StorageDevice} from '../Classes/StorageDevice';
+import {Tests} from '../Classes/Tests';
+import {Motherboard} from '../Classes/Motherboard';
+import {BIOS} from '../Classes/BIOS';
+import {IPMI} from '../Classes/IPMI';
+
 
 function Home() {
-  const [rows, setRows] = useState([])
-  const [columns, setColumns] = useState([])
-  useEffect(() => { setRows([
-    { id: 1, A: "c1", Customer: "The official united states of america and the outlying states", NodeName: "node1", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
+  const [singleNode, setSingleNode] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  function createSingleNode() {
+    console.log("fdsf")
+    const singleNode1 = new SingleNode("c1", "P&G", "node1", "123", "Idle", "0.0.0.0.1", "0.0.0.0.2", "0.0.0.0.3", "OK", "OK", false, createCPU(), "2952 MB", "0.54, 0.39, 0.36", "0 days, 0 hours, 32 minutes", "IPMI Log", createTests(), createBOM());
+    return singleNode1;
+  }
+
+  function createCPU() {
+    const CPU1 = new CPU("AMD Epyc", "2700 MHZ", "2", "64");
+    return CPU1;
+  }
+  
+    function createTests() {
+      const testArray = createTestsParentClass();
+  
+      const Tests1 = new Tests(testArray[0], testArray[1], testArray[2], testArray[3], testArray[4], testArray[5], testArray[6], testArray[7], testArray[8], testArray[9])
+      return Tests1;
+    }
+
+  function createTestsParentClass() {
+    const testArrayNames = ["Disk", "Memory", "CPUHPL", "GPU", "Network", "EOD", "Cycle", "USB", "CDROM", "IPMI"];
+    const testArray = [];
+
+    for (var i = 0; i < testArrayNames.length; i++) {
+      testArray.push(new TestParent(testArrayNames, "Status", "StartTime", "EndTime", "Cycles", "Log"));
+    }
+    return testArray;
+
+  }
+
+  function createBOM() {
+    const BOM1 = new BOM("Summary", createSystem(), createMotherboard(), createCPU(), createBIOS(), createMemInfo(), "GPU", createStorageDevice(), "Storage Controller",  "Network", "Display", "None found!", createIPMI() ) //might need to make a class for GPU, Network
+    return BOM1; 
+  }
+
+  function createSystem() {
+    const system1 = new System("ASUS", "RS720QA-E12-RS8U", "R5S0MD0003SH");
+    return system1;
+  }
+
+  function createMotherboard() {
+    const motherboard1 = new Motherboard("ASUSTeK COMPUTER INC. ", "K14PH-D24", "230418594500075");
+    return motherboard1;
+  }
+
+  function createBIOS() {
+    const BIOS1 = new BIOS("American Megatrends Inc. ", "0303 ", "03/03/2023 ");
+    return BIOS1;
+  }
+
+  function createMemInfo() {
+    const MemInfo1 = new MemInfo("755.6 GB", createDIMMList());
+    return MemInfo1;
+  }
+
+  function createDIMMList() {
+    const DIMMList = [];
+    const DIMM1 = new DIMM("32 GB", "CPU1_DIMM_A1", "4800 MT/s", "Kingston", "40177E8F", "9965791-087.A00G", "4800 MT/s");
+    for (var i = 0; i < 23; i++) {
+      DIMMList.push(DIMM1);
+    }
+
+    return DIMMList;
+  }
+
+  function createStorageDevice() {
+    const storageDevice1 = new StorageDevice("/dev/nvme0n1", "KINGSTON SEDC1000BM8480G", "Regular/USB = 0  NVME = 1  CD/DVD = 1480 GB", "Total NVME of size 447.1G = 1") //this is going to need a subclass of harddrive
+    return storageDevice1;
+  }
+
+  function createIPMI() {
+    const IPMI1 = new IPMI("KCS (Keyboard Control Style)", "2.0", "0x10", "0", "0x0000000000000CA2 (I/O)", "Successive Byte Boundaries");
+    return IPMI1;
+  }
+
+  useEffect(() => { 
+    setSingleNode(createSingleNode());
+    setRows([
+    { id: 1, A: singleNode.getAHash,Customer: singleNode.getCustomer, NodeName: singleNode.getNodeName, JobNum: singleNode.getJobNum, Status: singleNode.getStatus, VLAN: singleNode.getVLAN, BMCMAC: singleNode.getBMCMAC, BMCIP: singleNode.getBMCIP, MCE: singleNode.getMCE, SUM: singleNode.getSUM }, //still needs work
     { id: 2, A: "c2", Customer: "P&G", NodeName: "node2", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 3, A: "c3", Customer: "P&G", NodeName: "node3", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 4, A: "c4", Customer: "P&G", NodeName: "node4", JobNum: "124", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 5, A: "c5", Customer: "P&G", NodeName: "node5", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 6, A: "c6", Customer: "P&G", NodeName: "node6", JobNum: "124", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 7, A: "c7", Customer: "P&G", NodeName: "node7", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 8, A: "c8", Customer: "P&G", NodeName: "node8", JobNum: "124", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 9, A: "c9", Customer: "P&G", NodeName: "node9", JobNum: "123", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 10, A: "c10", Customer: "P&G", NodeName: "node10", JobNum: "124", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
-    { id: 11, A: "c11", Customer: "P&G", NodeName: "node11", JobNum: "124", Status: "Idle", VLAN: "0.0.0.0.0", BMCMAC: "0.0.0.0.0", BMCIP: "0.0.0.0.0", MCE: "OK", SUM: "OK" },
   ])
-    setColumns([
+  setColumns([
     { field: 'A', headerName: 'A#(WEB)', width: 115, flex: 0, headerAlign: "center", align: "center", headerClassName: 'super-app-theme--header' },
     { field: 'Customer', headerName: 'Customer', flex: 1, headerAlign: "center", align: "center", headerClassName: 'super-app-theme--header' },
     { field: 'NodeName', headerName: 'NodeName', editable: true, flex: 1, headerAlign: "center", align: "center", headerClassName: 'super-app-theme--header' },
@@ -31,8 +113,8 @@ function Home() {
     { field: 'MCE', headerName: 'MCE/EDAC', flex: 1, headerAlign: "center", align: "center", headerClassName: 'super-app-theme--header' },
     { field: 'SUM', headerName: 'SUM', flex: 1, headerAlign: "center", align: "center", headerClassName: 'super-app-theme--header' },
   ])});
-
-
+  
+  
   useEffect(() => {
     document.title = "AIME 2.0";
   }, [])

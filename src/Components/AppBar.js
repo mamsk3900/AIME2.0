420d748db66573a4bd2a908c07fad389b2cf1cf7
Sam McKay <sammckay31@gmail.com>
2024-10-28 10:48:57 -0600

    Add darkmode switch because I like how it looked on my other branch

--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -4,13 +4,16 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-
+import DarkModeSwitch from './DarkModeSwitch';
+import {useState, useEffect} from 'react';
+import { Tooltip } from '@mui/material';
 import Logo from '../imgs/logo.png';
 
 const pages = ['RT', 'Defective Material', "Wiki", "Sales Documents"];
 const pagesURLS = ["http://support.private.aspsys.com/", "http://aime.private.aspsys.com/defective_material.php", "http://wiki.private.aspsys.com/doku.php", "http://aime.private.aspsys.com/salesdocs/"  ];
 
 function ResponsiveAppBar() {
+  const [checked, setChecked] = useState();
   function handleButtonClicks(element){
     if (element != window.location.href) {
       window.open(pagesURLS[pages.indexOf(element)], "_blank");
@@ -20,6 +23,24 @@ function ResponsiveAppBar() {
  
   };
 
+  useEffect(() => {
+    if (!localStorage.getItem("theme")) {
+      localStorage.setItem("theme", "light");
+    } else if (localStorage.getItem("theme") === "dark") {
+      setChecked(false);
+    } else if (localStorage.getItem("theme") === "light") {
+      setChecked(true);
+    }
+  })
+
+
+  function handleThemeChange() {
+    if (localStorage.getItem("theme") === "light") {
+      return localStorage.setItem("theme", "dark");
+    } if (localStorage.getItem("theme") === "dark") {
+      return localStorage.setItem("theme", "light");
+    }
+  }
 
   return (
     <AppBar position="relative" width="100%">
@@ -27,6 +48,7 @@ function ResponsiveAppBar() {
         <Toolbar disableGutters>
           <img src={Logo} id="AspenLogo" alt='The Aspen Systems, Inc. Logo. A retro logo with a blue A with the words Aspen on the left, Systems on the right in magenta'/> 
           <Box id="NavItems" >
+            <DarkModeSwitch onClick={() => handleThemeChange()} sx={{marginBottom: "8%", overflow: "visible", width: "70px"}} checked={checked}></DarkModeSwitch>
             {pages.map((page) => (
               <Button
                 id="AppBarLinks"
--- src/Components/NodePageAppBar.js
+++ src/Components/NodePageAppBar.js
@@ -7,11 +7,13 @@ import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
 
 import Logo from '../imgs/logo.png';
+import DarkModeSwitch from './DarkModeSwitch';
 
 const pages = ['Home', 'Control Panel', 'Bill of Materials', "Reboot", "Power Off"]; //In this branch I really want to fix the issue where it opens the same window in a new tab if you click the link. There are some other link paths that need to be fixed
 
 function NodePageAppBar() {
   const [pagesURLS, setPageUrls] = useState([]);
+  const [checked, setChecked] = useState();
 
   function handleButtonClicks(element){
       window.open(pagesURLS[pages.indexOf(element)], "_self");
@@ -20,8 +22,26 @@ function NodePageAppBar() {
   useEffect(() => {
     let id = localStorage.getItem("nodeAHash");
     setPageUrls(["/NodePage"+id, "/", "/Bom"+id, "/", "/",  ])
+
+    if (!localStorage.getItem("theme")) {
+    } else if (localStorage.getItem("theme") === "dark") {
+      setChecked(false);
+
+    } else if (localStorage.getItem("theme") === "light") {
+      setChecked(true);
+
+    }
   })
 
+  function handleThemeChange() {
+    if (localStorage.getItem("theme") === "light") {
+      setChecked(false);
+      return localStorage.setItem("theme", "dark");
+    } if (localStorage.getItem("theme") === "dark") {
+      setChecked(true);
+      return localStorage.setItem("theme", "light");
+    }
+  }
 
   return (
     <AppBar position="relative" width="100%">
@@ -29,6 +49,7 @@ function NodePageAppBar() {
         <Toolbar disableGutters>
           <img src={Logo} id="AspenLogo" alt='The Aspen Systems, Inc. Logo. A retro logo with a blue A with the words Aspen on the left, Systems on the right in magenta'/> 
           <Box id="NavItems" >
+            <DarkModeSwitch onClick={() => handleThemeChange()} sx={{marginBottom: "7%", overflow: "visible", width: "70px"}} checked={checked}/>
             {pages.map((page) => (
               <Button
                 id="AppBarLinks"

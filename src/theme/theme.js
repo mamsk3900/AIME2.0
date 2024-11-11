92a8c73b55a29d51ca6469d779f7ac2366ced88a
Sam McKay <sammckay31@gmail.com>
2024-11-11 09:49:42 -0700

    Create the basis for the theme

---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+import {PaletteMode} from "@mui/material";
+import { amber, deepOrange, grey } from "@mui/material/colors";
+
+const theme = {
+    palette: {
+        primary: amber,
+    },
+};
+
+export default theme;

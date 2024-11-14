98a8ddecf1a53c810ef2389918dc728e83e4c1a7
Sam McKay <sammckay31@gmail.com>
2024-11-14 11:57:22 -0700

    Since I added ThemeProvider I can get rid of the useEffect hook that changes the header of the datatable. There are still some things to iron out in this file though

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { Paper } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { darken, lighten, styled } from '@mui/material/styles';
+import { useThemeContext } from "../theme/ThemeContextProvider";
 import { useState, useEffect } from 'react';
+import { Tooltip, ThemeProvider, CssBaseline } from '@mui/material';
 
 const getBackgroundColor = (color, theme, coefficient) => ({
   backgroundColor: darken(color, coefficient),
@@ -42,48 +44,35 @@ const StyledDataGrid = styled(DataGrid)(({ theme, oddTableEntryColor, evenTableE
 }));
 
 export default function DataTable({title, rows, columns, hideFooterBool, columnHeaderDisplayType}) {
-  const [dataTableHeaderColor, setDataTableHeaderColor] = useState("#1976D2");
-  const [oddTableEntryColor, setOddTableEntryColor] = useState("#1976D2");
+  const { theme } = useThemeContext();
+  const [oddTableEntryColor, setOddTableEntryColor] = useState("#ffffff");
   const [evenTableEntryColor, setEvenTableEntryColor] = useState("#ffffff");
-  const [titleColor, setTitleColor] = useState("black");
-  const [checked, setChecked] = useState("");
+
 
   useEffect(() => {
-    if (!localStorage.getItem("theme")) {
-      localStorage.setItem("theme", "light");
-      setDataTableHeaderColor("#1976D2");
-    } else if (localStorage.getItem("theme") === "dark") {
-      setChecked(false);
-      setDataTableHeaderColor("#1976D2");
-      setOddTableEntryColor("#ffffff");
-      setEvenTableEntryColor("#1976D2");
-      setTitleColor("black");
-    } else if (localStorage.getItem("theme") === "light") {
-      setChecked(true);
-      setDataTableHeaderColor("#0F0F0F");
-      setOddTableEntryColor("#ffffff");
-      setEvenTableEntryColor("#0F0F0F");
-      setTitleColor("white");
-    }
   })
 
   return (
-    <div id="DataGridDiv" >
-      <h2 id="AIMETableTitle" style={{textAlign: "center", color: titleColor}}>{title}</h2>
-      <Paper sx={{ height: "auto", width: '100%', margin:"auto", '& .super-app-theme--header': { backgroundColor: dataTableHeaderColor, color: "white" }}}>
-      <StyledDataGrid
-      autoHeight
-      disableRowSelectionOnClick
-      disableColumnResize
-      hideFooter={hideFooterBool}
-      columns={columns}
-      rows={rows}
-      oddTableEntryColor = {oddTableEntryColor}
-      evenTableEntryColor = {evenTableEntryColor}
-      sx={{ border: "2px solid lightgray", width: "auto", '& .MuiDataGrid-columnHeader': {display: columnHeaderDisplayType}}}
-      getRowClassName={(params) => `super-app-theme--${params.row.id % 2}`}      
-      />
-    </Paper>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div id="DataGridDiv" >
+        <h2 id="AIMETableTitle" style={{textAlign: "center", color: theme}}>{title}</h2>
+        <Paper sx={{ height: "auto", width: '100%', margin:"auto", '& .super-app-theme--header': { backgroundColor: theme}}}>
+        <StyledDataGrid
+        autoHeight
+        disableRowSelectionOnClick
+        disableColumnResize
+        hideFooter={hideFooterBool}
+        columns={columns}
+        rows={rows}
+        oddTableEntryColor = {oddTableEntryColor}
+        evenTableEntryColor = {evenTableEntryColor}
+        sx={{ border: "2px solid lightgray", width: "auto", '& .MuiDataGrid-columnHeader': {display: columnHeaderDisplayType}}}
+        getRowClassName={(params) => `super-app-theme--${params.row.id % 2}`}      
+        />
+      </Paper>
+      </div>
+
+    </ThemeProvider>
   );
 }

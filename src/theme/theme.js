62a1ae49edbc31d05f4b9196f762d5b7ba63253a
Sam McKay <sammckay31@gmail.com>
2024-11-14 11:57:43 -0700

    Refine some of the colors. Also get used to some of the syntax that this uses.

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,10 +1,11 @@
-import { amber, deepOrange, red, blueGrey } from "@mui/material/colors";
-
 const theme = {
   palette: {
     primary: {
       main: "#1976D2"
     },  // Default primary color (you can adjust this if needed)
+    secondary: {
+      main: "#282828"
+    }
   },
 };
 
@@ -15,23 +16,23 @@ export const getDesignTokens = (mode) => ({
       ? {
           primary: theme.primary,          // Primary color for light mode
           background: {
-            default: "#fafafa",  // Light background color
-            paper: "#ffffff",    // Paper color for light mode
+            default: "#f5f5f5",  // Light background color
+            paper: "#1976D2",    // Paper color for light mode
           },
           text: {
             primary: "#000000",  // Text color for light mode
-            secondary: "#757575",
+            secondary: "#000000",
           },
         }
       : {
-          primary: theme.primary,       // Primary color for dark mode
+          primary: theme.secondary,       // Primary color for dark mode
           background: {
             default: "#121212",  // Dark background color
             paper: "#1D1D1D",    // Paper color for dark mode
           },
           text: {
             primary: "#ffffff",  // Text color for dark mode
-            secondary: "#B0B0B0",
+            secondary: "#ffffff",
           },
         }),
   },

e8331824f32857bac780ffdbe5d1a7600a12ddb4
Sam McKay <sammckay31@gmail.com>
2024-11-15 15:58:50 -0700

    Make more changes to the getDesign token function

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,40 +1,22 @@
-const theme = {
-  palette: {
-    primary: {
-      main: "#1976D2",
-      sub: "#ffffff"
-    },  
-    secondary: {
-      main: "#000000"
-    }
-  },
-};
-
 export const getDesignTokens = (mode) => ({
   palette: {
     mode,
     ...(mode === "light"
       ? {
           primary: {
-            main: "#1976D2",  // Primary color for light mode
-            sub: "#ffffff"
+            main: "#197d3",
           },
           text: {
             primary: "#000000",  // Text color for light mode
-            secondary: "#000000",
           },
         }
       : {
           primary: {
-            main: "#ba0b0b",  // Primary color for dark mode
-            sub: "#ffffff"
+            main: "#0a0a0a"
           },
           text: {
             primary: "#ffffff",  // Text color for dark mode
-            secondary: "#ffffff",
           },
         }),
   },
 });
-
-export default theme;

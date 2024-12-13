423b4ce6edb73145d1acb050811d1f8896e10b6f
Sam McKay <sammckay31@gmail.com>
2024-12-12 17:37:01 -0700

    Mess around with this to see if it does anything... I don't know

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,14 +5,22 @@ export const getDesignTokens = (mode) => ({
       ? {
           primary: {
             main: "#197d3",
+            sub: "#fff",
           },
           text: {
             primary: "#000000",  // Text color for light mode
           },
+          body: {
+            primary: "#929292"
+          },
+          bodyBackground: {
+            main: "red",
+          }
         }
       : {
           primary: {
-            main: "#0a0a0a"
+            main: "#0a0a0a",
+            sub: "#000"
           },
           text: {
             primary: "#ffffff",  // Text color for dark mode
